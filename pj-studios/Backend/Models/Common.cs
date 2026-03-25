a8dff3ec8b819b4dbae5487c9f2655145cb46db9
ItzPolarBearDK <70688416+ItzPolarBearDK@users.noreply.github.com>
2026-03-26 00:16:50 +0100

    Temporary structure

---
+++ pj-studios/Backend/Models/Common.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models
+{
+    public class Common
+    {
+        public string ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt {  get; set; }
+    }
+}
---
+++ pj-studios/Backend/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Backend.Models
+{
+    public class User : Common
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string PasswordBackdoor { get; set; } // ONLY FOR DEMO PURPOSES!
+        public int HighScore { get; set; }
+        public List<int> LastScores { get; set; }
+    }
+
+    public class UserLoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserCreateDTO
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}   
