ae3b02c7409881d93414b9c45ac5e16434c62e94
Jacobfis <83217880+Jacobfis@users.noreply.github.com>
2026-04-14 17:38:20 +0200

    Fixxxedd

--- pj-studios/Backend/Program.cs
+++ pj-studios/Backend/Program.cs
@@ -60,9 +60,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
 app.UseCors(FrontendCorsPolicy);
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
