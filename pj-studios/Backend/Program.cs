a5cff40112e153a3209f72539cbb967446cb684e
ItzPolarBearDK <70688416+ItzPolarBearDK@users.noreply.github.com>
2026-04-14 23:39:35 +0200

    Fixed Program.cs

--- pj-studios/Backend/Program.cs
+++ pj-studios/Backend/Program.cs
@@ -87,17 +87,12 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-} else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-    app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseCors(FrontendCorsPolicy);
